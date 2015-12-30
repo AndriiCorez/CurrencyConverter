2ed7078908136d0ced465dc890eb226c28bbd8be
Andres <andres@Andress-Mac.local>
2015-12-30 08:50:41 -0800

    first commit

--- CurrencyConverter/ViewController.m
+++ CurrencyConverter/ViewController.m
@@ -7,21 +7,41 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *fieldInput;
+@property (weak, nonatomic) IBOutlet UILabel *currency1;
+@property (weak, nonatomic) IBOutlet UILabel *currency2;
+@property (weak, nonatomic) IBOutlet UILabel *currency3;
+@property (weak, nonatomic) IBOutlet UIButton *btnConvert;
+@property (nonatomic) CRCurrencyRequest *request;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)btnTapped:(id)sender {
+    self.btnConvert.enabled = NO;
+    
+    self.request = [[CRCurrencyRequest alloc] init];
+    self.request.delegate = self;
+    [self.request start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req retrievedCurrencies:(CRCurrencyResults *)currencies{
+    
+    double inputValue = [self.fieldInput.text floatValue];
+    double eur = inputValue * currencies.EUR;
+    double gbp = inputValue * currencies.GBP;
+    double pln = inputValue * currencies.PLN;
+    
+    self.currency1.text = [NSString stringWithFormat:@"%.2f", eur];
+    self.currency2.text = [NSString stringWithFormat:@"%.2f", gbp];
+    self.currency3.text = [NSString stringWithFormat:@"%.2f", pln];
+    self.btnConvert.enabled = YES;
 }
+
 
 @end
